efbb3704458395b389212118b01da0a627c84321
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-12 12:55:09 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# sync-shipengine-to-moltin
+# @particular./sync-shipengine-to-moltin
+
+[![npm version](https://badge.fury.io/js/%40particular.%2Fsync-shipengine-to-moltin.svg)](https://badge.fury.io/js/%40particular.%2Fsync-shipengine-to-moltin)
+
+> Update Moltin order shipping status when delivered in ShipEngine
+
+Asynchronous microservice that is triggered by [ShipEngine](https://www.shipengine.com/) webhooks to update Order data inside of [moltin](https://moltin.com).
+
+Built with [Micro](https://github.com/zeit/micro)! 🤩
+
+## 🛠 Setup
+
+Both a [moltin](https://moltin.com) _and_ [ShipEngine](https://www.shipengine.com/) account are needed for this to function.
+
+Create a `.env` at the project root with the following credentials:
+
+```dosini
+MOLTIN_CLIENT_ID=
+MOLTIN_CLIENT_SECRET=
+```
+
+Find your `MOLTIN_CLIENT_ID` and `MOLTIN_CLIENT_SECRET` inside of your [moltin Dashboard](https://dashboard.moltin.com)'s API keys.
+
+## 📦 Package
+
+Run the following command to build the app
+
+```bash
+yarn install
+```
+
+Start the development server
+
+```bash
+yarn dev
+```
+
+The server will typically start on PORT `3000`, if not, make a note for the next step.
+
+Start ngrok (change ngrok port below from 3000 if yarn dev deployed locally on different port above)
+
+```bash
+ngrok http 3000
+```
+
+Make a note of the https `ngrok URL` provided.
+
+## ⛽️ Usage
+
+Next head over to the ShipEngine [API Management>Webhooks](https://app.shipengine.com/#/portal/apimanagement) area, add a new webhook with the following details:
+
+| Events             | Webhook URL        | Status |
+| ------------------ | ------------------ | ------ |
+| Any tracking event | `ngrok URL` above_ |   On   |
+
+## 🚀 Deploy
+
+You can easily deploy this function to [now](https://now.sh).
+
+_Contact [Adam Grohs](https://www.linkedin.com/in/adamgrohs/) @ [Particular.](https://uniquelyparticular.com) for any questions._
---
+++ index.js
@@ -0,0 +1,128 @@
+const { json, send } = require('micro')
+const { MemoryStorageFactory } = require('@moltin/sdk')
+const moltinGateway = require('@moltin/sdk').gateway
+const moltin = moltinGateway({
+  client_id: process.env.MOLTIN_CLIENT_ID,
+  client_secret: process.env.MOLTIN_CLIENT_SECRET,
+  storage: new MemoryStorageFactory(),
+  application: 'demo-sync-shipengine-to-moltin'
+})
+const cors = require('micro-cors')({
+  allowMethods: ['POST']
+})
+
+const _toJSON = error => {
+  return !error
+    ? ''
+    : Object.getOwnPropertyNames(error).reduce(
+        (jsonError, key) => {
+          return { ...jsonError, [key]: error[key] }
+        },
+        { type: 'error' }
+      )
+}
+
+const _toLowercase = string => {
+  return !string ? '' : string.toLocaleLowerCase()
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(
+    'Promise unhandledRejection: ',
+    p,
+    ', reason:',
+    JSON.stringify(reason)
+  )
+})
+
+module.exports = cors(async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return send(res, 204)
+  }
+
+/*
+{
+  "resource_url": "https://api.shipengine.com/v1/tracking/usps/9361269903502070406152",
+  "resource_type": "API_TRACK",
+  "data": {
+    "links": {
+      "self": {
+        "href": "https://api.shipengine.com/v1/tracking/usps/9361269903502070406152"
+      },
+      "label": null
+    },
+    "tracking_number": "9361269903502070406152",
+    "status_code": "DE",
+    "status_description": "Delivered",
+    "carrier_status_code": "01",
+    "carrier_status_description": "Your item was picked up at the post office...",
+    "shipped_date": "2019-04-12T05:00:00.000Z",
+    "estimated_delivery_date": null,
+    "actual_delivery_date": "2019-04-12T17:45:26.734Z",
+    "exception_description": null,
+    "events": [
+      {
+        "event_date": "2019-04-12T17:45:26.734Z",
+        "description": "Delivered, Individual Picked Up at Post Office",
+        "city_locality": "AUSTIN",
+        "state_province": "TX",
+        "postal_code": "78721",
+        "country_code": "",
+        "company_name": "",
+        "signer": ""
+      }
+    ]
+  }
+}
+*/
+
+  try {
+    const tracking_update = await json(req)
+    const {
+      status_description: status
+    } = tracking_update
+
+    let tracking_extra = {} //TODO: figure out how to get OrderID added to ShipEngine Shipment
+    if (tracking_update.extra) {
+      tracking_extra = tracking_update.extra
+    }
+    const { order_id } = tracking_extra
+    // let { order_id } = tracking_extra
+
+    // if (!order_id) {
+    //   order_id = '730589c9-ee68-44b4-a201-bb38a9468abe'
+    // }
+
+    if (order_id) {
+      if (_toLowercase(status) === 'delivered') {
+        moltin.Orders.Update(order_id, {
+          shipping: 'fulfilled'
+        })
+          .then(order => {
+            console.info(order)
+            return send(res, 200, JSON.stringify({ received: true, order_id }))
+          })
+          .catch(error => {
+            const jsonError = _toJSON(error)
+            return send(
+              res,
+              jsonError.errors[0].status ? jsonError.errors[0].status : 500,
+              jsonError
+            )
+          })
+      } else {
+        return send(res, 200, JSON.stringify({ received: true, order_id }))
+      }
+    } else {
+      console.error('missing order_id')
+      return send(
+        res,
+        200,
+        JSON.stringify({ received: true, order_id: 'null' })
+      )
+    }
+  } catch (error) {
+    const jsonError = _toJSON(error)
+    return send(res, 500, jsonError)
+  }
+})
